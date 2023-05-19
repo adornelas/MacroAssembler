11ec49cdc52c78ff15641bc9b985ab157981db7d
André <andredornelas23@gmail.com>
2023-05-19 20:42:55 -0300

    Adicionando headers de datatype e preprocessor

---
+++ include/datatypes.hpp
@@ -0,0 +1,32 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <algorithm>
+
+struct fileData {
+    std::string name;
+    std::string content;
+};
+
+struct tokenMatrix {
+    int lines;
+    std::vector<std::vector<std::string>> matrix;
+};
+
+struct equIdentifier {
+    std::string name;
+    int value;
+};
+
+void ConvertFileToMatrix(fileData * input_file, tokenMatrix * input_matrix);
+void ConvertFileToMatrixCaps(fileData * input_file, tokenMatrix * input_matrix);
+void ConvertMatrixToFile(tokenMatrix * output_matrix, fileData * output_file);
+
+std::vector <std::string> Split(std::string text, char separator);
+std::vector <std::string> CommonSplit(std::string text, char separator);
+
+#endif
---
+++ include/preprocessor.hpp
@@ -0,0 +1,8 @@
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
+
+#include "datatypes.hpp"
+
+void PreProcessing(fileData * input_file, fileData * outuput_file);
+
+#endif
