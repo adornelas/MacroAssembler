6c8dc9cf955b7bf074bcbbaff92df7b87cbbda0b
André <andredornelas23@gmail.com>
2023-05-21 22:22:58 -0300

    Agora argumento deve ser o arquivo sem a extensão

--- montador.cpp
+++ montador.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    std::string teste;
     if (argc != 2)
     {
         cout << "Quantidade de argumentos invalida (" << argc << "),\ninsira no programa -diretiva nomedoprograma.asm nomedasaida.obj" << endl;
@@ -17,9 +18,10 @@ int main(int argc, char **argv)
     }
 
     fileData *input_file = new fileData{.name = argv[1]};
+    input_file->name.append(".asm");
     fileData *output_file = new fileData{.name = "preprocessedfile"};
 
-    ifstream ifs(argv[1]);
+    ifstream ifs(input_file->name);
     stringstream buffer;
     buffer << ifs.rdbuf();
     input_file->content = buffer.str();
