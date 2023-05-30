300891640e9747a027c762682b7f85e902eae2a0
André <andredornelas23@gmail.com>
2023-05-29 21:32:27 -0300

    Adiciona headers do tradutor

---
+++ include/translator.hpp
@@ -0,0 +1,13 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+#include "datatypes.hpp"
+#include <iostream>
+
+extern std::map<std::string, char> op_type_map;
+extern std::map<std::string, int> op_size_map;
+extern std::map<std::string, std::string> op_code_map;
+
+void Translator(fileData * input_file, fileData * outuput_file);
+
+#endif
