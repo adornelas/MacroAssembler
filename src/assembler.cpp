f2d56e23267e6e71d5eced64896541f0c0ed92cb
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-06-01 10:25:15 -0300

    implementa estrutura principal do assembler

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -1,5 +1,18 @@
 #include "../include/assembler.hpp"
 
 void Assembler(fileData *input_file, fileData *output_file){
-    printf("tudo certo\n");
+    
+    tokenMatrix *input_matrix = new tokenMatrix{.lines = 0};
+    tokenMatrix *output_matrix = new tokenMatrix{.lines = 0};
+
+    ConvertFileToMatrix(input_file, input_matrix);
+    TranslateAssemblyToObject(input_matrix);
+    ConvertMatrixToFile(input_matrix, output_file);
+
+    delete input_matrix;
+    delete output_matrix;
+}
+
+void TranslateAssemblyToObject(tokenMatrix *input_matrix){
+
 }
