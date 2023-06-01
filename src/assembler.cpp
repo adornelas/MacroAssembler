b38c21c70355e7db0f5b5aabf0d5a1c1100eb17b
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-06-01 15:45:25 -0300

    implementa logica de verificacao para rotulo e simbolos (sem testar)

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -43,8 +43,28 @@ bool isSymbolDefined(std::vector<symbolData> symbol_table, std::string symbol, i
     return is_symbol_defined;
 }
 
-void isInstruction(std::string token){
+bool isInstructionOrDirective(std::string token){
+    
+    if(op_size_map.find(token) == op_size_map.end()){
+        return true;
+    }
 
+    return false;
+}
+
+bool isNumber(std::string token){
+    char *p;
+    strtol(token.c_str(), &p, 10);
+    return *p == 0;
+
+}
+
+void insertOnListOfDependecies(std::vector<symbolData> symbol_table, int symbol_address, int token_address){
+   symbol_table[symbol_address].list_of_dependencies.insert(symbol_table[symbol_address].list_of_dependencies.end(), token_address );
+}
+
+void insertOnSymbolTable(std::vector<symbolData> symbol_table, symbolData symbol_data ){
+    symbol_table.insert(symbol_table.end(), symbol_data);
 }
 
 void TranslateAssemblyToObject(tokenMatrix *input_matrix){
@@ -55,32 +75,37 @@ void TranslateAssemblyToObject(tokenMatrix *input_matrix){
     for(int i = 0; i < input_matrix->matrix.size(); i++){
         matrix_line = input_matrix->matrix[i];
 
-        if(isLabel(matrix_line[0])){
-            if(isSymbolOnSymbolTable(symbol_table, matrix_line[0], symbol_address)){
-                if(isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
-                    printf("Erro: rotulo duplicado");
-                }
+        for(int j = 0; j < matrix_line.size(); j++){
 
-                if(!isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
+            if(isLabel(matrix_line[j])){
+                if(isSymbolOnSymbolTable(symbol_table, matrix_line[0], symbol_address)){
+                    if(isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
+                        printf("Erro: rotulo duplicado");
+                    }
+
                     symbol_table[symbol_address].is_defined = true;
                     
                     // #TODO: substituir o valor de acordo com a lista de dependencias
                 }
+
+                insertOnSymbolTable(symbol_table, {matrix_line[j], j, true});
             }
 
-            symbol_table.insert(symbol_table.end(), {matrix_line[0], i, .is_defined = true});
+            if(!isInstructionOrDirective(matrix_line[j]) && !isNumber(matrix_line[j])){
+                if(isSymbolOnSymbolTable(symbol_table, matrix_line[0], symbol_address)){
+                    if(isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
+                        // #TODO: substituir valor do simbolo no codigo
+                    }
+                        
+                    insertOnListOfDependecies(symbol_table, symbol_address, j);
+                }
+
+                insertOnSymbolTable(symbol_table, {matrix_line[j], j, false});
+
+            }
+
+            // aqui ele pode ser operando, diretiva ou numero 
         }
 
-
-        // nao é rotulo
-            // se nao é instrucao nem diretiva é operando
-            // se operando nao é numero
-                // se acha operando na TS
-                    // se é indefinido
-                        // coloca address na lista de dependencias
-                    // se é definido
-                        // substitui o valor do simbolo no codigo
-                // se nao achou 
-                    // coloca simbolo na TS 
     }
 }
