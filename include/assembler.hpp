849fbe3c8a3daa9c5adb5083bd001e46b2f6a399
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-06-01 10:10:49 -0300

    cria modulo para assembler

---
+++ include/assembler.hpp
@@ -0,0 +1,9 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+#include "translator.hpp"
+#include "datatypes.hpp"
+
+void Assembler(fileData *input_file, fileData *output_file);
+
+#endif
---
+++ src/assembler.cpp
@@ -0,0 +1,5 @@
+#include "../include/assembler.hpp"
+
+void Assembler(fileData *input_file, fileData *output_file){
+    printf("tudo certo\n");
+}
