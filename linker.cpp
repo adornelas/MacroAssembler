a89c8c5b82643410d590b9da65fc2230a7904d41
André <andredornelas23@gmail.com>
2023-06-02 11:59:37 -0300

    corrige APENAS formatação

--- linker.cpp
+++ linker.cpp
@@ -9,22 +9,19 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     vector<tokenMatrix> input_matrix(argc - 1);
 
-    if (argc < 2 && argc > 5)
-    {
+    if (argc < 2 && argc > 5) {
         cout << "Quantidade de argumentos invalida (" << argc << "),\ninsira até 4 arquivos no programa: nomedoprograma1.asm nomedoprograma2.asm nomedoprograma3.asm nomedoprograma4.asm" << endl;
         return 1;
     }
 
-
     // TODO: aceitar arquivos de entrada sem extensão
-    for (int i = 1; i < argc; i++)
-    {
-        fileData *input_file = new fileData{.name = argv[i]};
-        input_matrix.push_back({.lines = 0});
+    for (int i = 1; i < argc; i++) {
+        fileData* input_file = new fileData{ .name = argv[i] };
+        input_matrix.push_back({ .lines = 0 });
+
         input_file->name.append(".obj");
         ifstream ifs(input_file->name);
         stringstream buffer;
@@ -34,7 +31,7 @@ int main(int argc, char **argv)
         delete input_file;
     }
 
-    fileData *output_file = new fileData{.name = argv[1]};
+    fileData* output_file = new fileData{ .name = argv[1] };
     output_file->name.append(".exc");
 
     // ligador
--- src/preprocessor.cpp
+++ src/preprocessor.cpp
@@ -1,10 +1,9 @@
 #include "../include/preprocessor.hpp"
 
 // #TODO: Lidar com diretiva CONST (verificar se está no formato correto e passar para decimal)
-void PreProcessing(fileData *input_file, fileData *output_file)
-{
-    tokenMatrix *input_matrix = new tokenMatrix{.lines = 0};
-    tokenMatrix *output_matrix = new tokenMatrix{.lines = 0};
+void PreProcessing(fileData* input_file, fileData* output_file) {
+    tokenMatrix* input_matrix = new tokenMatrix{ .lines = 0 };
+    tokenMatrix* output_matrix = new tokenMatrix{ .lines = 0 };
 
     ConvertFileToMatrixCaps(input_file, input_matrix);
     CleanMatrix(input_matrix);
@@ -14,37 +13,37 @@ void PreProcessing(fileData *input_file, fileData *output_file)
     delete output_matrix;
 }
 
-void CleanMatrix(tokenMatrix * input_matrix){
+void CleanMatrix(tokenMatrix* input_matrix) {
     std::vector<std::string> matrix_line;
     std::vector<std::string> clean_line;
     std::vector<std::vector<std::string>> section_data;
 
-    for(int i = 0; i < input_matrix->lines; i++){
+    for (int i = 0; i < input_matrix->lines; i++) {
         matrix_line = input_matrix->matrix[i];
 
         clean_line = {};
 
-        for (long unsigned int j = 0; j < matrix_line.size(); j++){
+        for (long unsigned int j = 0; j < matrix_line.size(); j++) {
             if (matrix_line[j][0] == ';')
                 break;
-            
+
             clean_line.push_back(matrix_line[j]);
         }
         input_matrix->matrix[i] = clean_line;
-        matrix_line = input_matrix->matrix[i];        
-        
-        if(matrix_line.size() == 0){
+        matrix_line = input_matrix->matrix[i];
+
+        if (matrix_line.size() == 0) {
             input_matrix->matrix.erase(input_matrix->matrix.begin() + i);
             input_matrix->lines--;
             i--;
         }
     }
 
-    for (int i = 0; i < input_matrix->lines; i++){
+    for (int i = 0; i < input_matrix->lines; i++) {
         matrix_line = input_matrix->matrix[i];
 
-        if(matrix_line.size() == 1 && isLabel(matrix_line[0])){
-            matrix_line.insert(matrix_line.end(), input_matrix->matrix[i+1].begin(), input_matrix->matrix[i+1].end());
+        if (matrix_line.size() == 1 && isLabel(matrix_line[0])) {
+            matrix_line.insert(matrix_line.end(), input_matrix->matrix[i + 1].begin(), input_matrix->matrix[i + 1].end());
             input_matrix->matrix.erase(input_matrix->matrix.begin() + i + 1);
         }
 
@@ -52,26 +51,27 @@ void CleanMatrix(tokenMatrix * input_matrix){
 
     bool isInSectionData = false;
 
-    for (int i = 0; i < input_matrix->lines; i++){
+    for (int i = 0; i < input_matrix->lines; i++) {
         matrix_line = input_matrix->matrix[i];
 
-        if(isInSectionData) {
-            for(int j = 0; j < input_matrix->matrix[i].size(); j++){
-                if(input_matrix->matrix[i][j] == "SECTION" && input_matrix->matrix[i][j+1] == "TEXT" ){
+        if (isInSectionData) {
+            for (int j = 0; j < input_matrix->matrix[i].size(); j++) {
+                if (input_matrix->matrix[i][j] == "SECTION" && input_matrix->matrix[i][j + 1] == "TEXT") {
                     isInSectionData = false;
                     break;
-                }               
+                }
             }
 
-            if(isInSectionData) {
+            if (isInSectionData) {
                 section_data.push_back(input_matrix->matrix[i]);
                 input_matrix->matrix.erase(input_matrix->matrix.begin() + i);
                 i--;
             }
 
-        } else {
-            for(int j = 0; j < input_matrix->matrix[i].size(); j++){
-                if(input_matrix->matrix[i][j] == "SECTION" && input_matrix->matrix[i][j+1] == "DATA"){
+        }
+        else {
+            for (int j = 0; j < input_matrix->matrix[i].size(); j++) {
+                if (input_matrix->matrix[i][j] == "SECTION" && input_matrix->matrix[i][j + 1] == "DATA") {
                     section_data.push_back(input_matrix->matrix[i]);
                     section_data.push_back(input_matrix->matrix[i + 1]);
                     input_matrix->matrix.erase(input_matrix->matrix.begin() + i);
@@ -82,7 +82,7 @@ void CleanMatrix(tokenMatrix * input_matrix){
         }
     }
 
-    for (int i = 0; i < section_data.size(); i++){
+    for (int i = 0; i < section_data.size(); i++) {
         input_matrix->matrix.push_back(section_data[i]);
     }
 
