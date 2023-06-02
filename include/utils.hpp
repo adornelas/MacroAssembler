c4673cc39b58091e495eaaaf0c3b0a94446863f2
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-06-02 19:20:43 -0300

    altera assinatura de funcoes e cria mais duas assinaturas

--- include/utils.hpp
+++ include/utils.hpp
@@ -12,11 +12,14 @@ void ConvertMatrixToFile(tokenMatrix * output_matrix, fileData * output_file);
 std::vector <std::string> Split(std::string text, char separator);
 std::vector <std::string> CommonSplit(std::string text, char separator);
 
-bool isSymbolOnSymbolTable(std::vector<symbolData> symbol_table, std::string symbol,  int symbol_address);
-bool isSymbolDefined(std::vector<symbolData> symbol_table, std::string symbol, int symbol_address);
+int isSymbolOnSymbolTable(std::vector<symbolData> &symbol_table, std::string symbol);
+bool isSymbolDefined(std::vector<symbolData> &symbol_table, std::string symbol);
 bool isInstructionOrDirective(std::string token);
 bool isNumber(std::string token);
-void insertOnListOfDependecies(std::vector<symbolData> symbol_table, int symbol_address, int token_address);
-void insertOnSymbolTable(std::vector<symbolData> symbol_table, symbolData symbol_data );
+bool isOperator(std::string token);
+void insertOnListOfDependecies(std::vector<symbolData> &symbol_table, int symbol_address, int token_address);
+void insertOnSymbolTable(std::vector<symbolData> &symbol_table, symbolData symbol_data );
+
+void ConvertArrayObjectToFile(std::vector<std::string> &output_object, fileData * output_file);
 
 #endif
