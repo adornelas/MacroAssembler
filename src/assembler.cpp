818a3574d254bddd8e940ce141fbe7e409fcb259
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-06-02 19:20:57 -0300

    implementa montador de uma passagem

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -3,55 +3,125 @@
 void Assembler(fileData *input_file, fileData *output_file){
     
     tokenMatrix *input_matrix = new tokenMatrix{.lines = 0};
-    tokenMatrix *output_matrix = new tokenMatrix{.lines = 0};
+    std::vector<std::string> output_object;
 
     ConvertFileToMatrix(input_file, input_matrix);
-    TranslateAssemblyToObject(input_matrix);
-    ConvertMatrixToFile(input_matrix, output_file);
+    TranslateAssemblyToObject(input_matrix, output_object);
+    ConvertArrayObjectToFile(output_object, output_file);
 
     delete input_matrix;
-    delete output_matrix;
 }
 
-void TranslateAssemblyToObject(tokenMatrix *input_matrix){
+void TranslateAssemblyToObject(tokenMatrix *input_matrix, std::vector<std::string> &output_object){
     std::vector<std::string> matrix_line;
     std::vector<symbolData> symbol_table;
-    int symbol_address;
+    std::string symbol_clean_name; // usado para guardar o rotulo sem ':'
+
+    int symbol_address = 0;
+    int current_address = 0;
+    int operand_quantity = 0; // usada para contar os operandos
+    int value = 0; // usada para auxiliar na contagem da posição do operando
+    int current_line_address = 0;
+    int current_line_size = 0;
+
+    // std::vector<std::string> object; 
+    int opcode;
+
+    std::vector<int> list_aux; // usada para auxiliar na criação da lista de dependencias
 
     for(int i = 0; i < input_matrix->matrix.size(); i++){
         matrix_line = input_matrix->matrix[i];
+        operand_quantity = 0;
 
         for(int j = 0; j < matrix_line.size(); j++){
-
-            if(isLabel(matrix_line[j])){
-                if(isSymbolOnSymbolTable(symbol_table, matrix_line[0], symbol_address)){
-                    if(isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
-                        printf("Erro: rotulo duplicado");
-                    }
-
-                    symbol_table[symbol_address].is_defined = true;
-                    
-                    // #TODO: substituir o valor de acordo com a lista de dependencias
-                }
-
-                insertOnSymbolTable(symbol_table, {matrix_line[j], j, true});
-            }
-
-            if(!isInstructionOrDirective(matrix_line[j]) && !isNumber(matrix_line[j])){
-                if(isSymbolOnSymbolTable(symbol_table, matrix_line[0], symbol_address)){
-                    if(isSymbolDefined(symbol_table, matrix_line[0], symbol_address)){
-                        // #TODO: substituir valor do simbolo no codigo
-                    }
                         
-                    insertOnListOfDependecies(symbol_table, symbol_address, j);
+            if(isLabel(matrix_line[j])){
+                symbol_clean_name = matrix_line[j];
+                symbol_clean_name.erase(remove(symbol_clean_name.begin(), symbol_clean_name.end(), ':'), symbol_clean_name.end());
+
+                if(isSymbolOnSymbolTable(symbol_table, symbol_clean_name) != -1){
+                    if(isSymbolDefined(symbol_table, symbol_clean_name)){
+                        printf("ERRO - rotulo duplicado\n");
+                    }
+                    else{
+                        
+                        symbol_address = isSymbolOnSymbolTable(symbol_table, symbol_clean_name);
+                        symbol_table[symbol_address].is_defined = true;
+                        symbol_table[symbol_address].value = current_line_address;
+                    
+                        for(int l = 0; l < symbol_table[symbol_address].list_of_dependencies.size(); l++){
+                            output_object[symbol_table[symbol_address].list_of_dependencies[l]] = std::to_string(symbol_table[symbol_address].value);
+                        }
+                    }
+                }else{
+                    insertOnSymbolTable(symbol_table, {symbol_clean_name, current_line_address, true});
+                }                
+            }
+            else if(isOperator(matrix_line[j])){ 
+                operand_quantity++;
+                value = current_line_address + operand_quantity;
+                
+                if(isSymbolOnSymbolTable(symbol_table, matrix_line[j]) != -1){
+                    if(isSymbolDefined(symbol_table, matrix_line[j])){
+                        symbol_address = isSymbolOnSymbolTable(symbol_table, symbol_clean_name);
+                        output_object.insert(output_object.end(), std::to_string(symbol_table[symbol_address].value));
+                    }
+                    else {
+                        symbol_address = isSymbolOnSymbolTable(symbol_table, matrix_line[j]);
+                        insertOnListOfDependecies(symbol_table, symbol_address, value);
+                    }
+                }
+                else {
+                    list_aux.clear();
+                    list_aux.insert(list_aux.end(), value);
+                    insertOnSymbolTable(symbol_table, {matrix_line[j], -1, false, list_aux});
+                    output_object.insert(output_object.end(), "-1");
+                }
+            }
+            else if(isInstructionOrDirective(matrix_line[j])){
+                current_line_size += op_size_map.find(matrix_line[j])->second;;
+
+                if((matrix_line[j].compare("CONST") != 0) && (matrix_line[j].compare("SPACE") != 0)){
+                    opcode = stol(op_code_map.find(matrix_line[j])->second);
+                    output_object.insert(output_object.end(), std::to_string(opcode));
                 }
 
-                insertOnSymbolTable(symbol_table, {matrix_line[j], j, false});
+                if((matrix_line[j].compare("CONST") == 0)){
+                    if(matrix_line.size() > j + 1) {
+                        output_object.insert(output_object.end(), matrix_line[j+1]);
+                    }
+                    else{
+                        printf("ERRO - CONST SEM VALOR");
+                    }
+                }
 
+                if((matrix_line[j].compare("SPACE") == 0)){
+                    if(matrix_line.size() > j + 1) {
+                        for(int m = 0; m < stol(matrix_line[j+1]); m++){
+                            output_object.insert(output_object.end(), "0");
+                        }
+                    }
+                    else{
+                        output_object.insert(output_object.end(), "0");
+                    }
+                }
             }
-
-            // aqui ele pode ser operando, diretiva ou numero 
         }
 
+        current_line_address += current_line_size;
+        current_line_size = 0;
+
+    }
+
+    bool symbol_found = false;
+    for(int i = 0; i < symbol_table.size(); i++){
+        if(!symbol_table[i].is_defined){
+            symbol_found = true;
+            break;
+        }
+    }
+
+    if(symbol_found){
+        printf("ERRO - o simbolo nao é definido\n ");
     }
 }
