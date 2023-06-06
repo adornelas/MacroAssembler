39b923fee2cfb10783c10571dba975c208d508b9
André <andredornelas23@gmail.com>
2023-06-06 19:01:25 -0300

    fluxo para apenas montar

--- montador.cpp
+++ montador.cpp
@@ -16,35 +16,37 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    fileData *input_file = new fileData{.name = argv[1]};
-    input_file->name.append(".asm");
-    fileData *output_file_pre_processing = new fileData{.name = "preprocessedFile"};
-    fileData *output_file_assembler = new fileData{.name = "assembledFile"};
+    if(argc == 2) {
+        fileData *input_file = new fileData{.name = argv[1]};
+        input_file->name.append(".asm");
+        fileData *output_file_pre_processing = new fileData{.name = "preprocessedFile"};
+        fileData *output_file_assembler = new fileData{.name = "assembledFile"};
 
-    ifstream ifs(input_file->name);
-    stringstream buffer;
-    buffer << ifs.rdbuf();
-    input_file->content = buffer.str();
+        ifstream ifs(input_file->name);
+        stringstream buffer;
+        buffer << ifs.rdbuf();
+        input_file->content = buffer.str();
 
-    // #TODO: Verificar se nos arquivos testes existe algum caso com arquivo vazio
-    if(input_file->content.length() <= 1){
-        printf("Arquivo %s inválido!\n", input_file->name.c_str());
-        return 1;
+        // #TODO: Verificar se nos arquivos testes existe algum caso com arquivo vazio
+        if(input_file->content.length() <= 1){
+            printf("Arquivo %s inválido!\n", input_file->name.c_str());
+            return 1;
+        }
+
+        PreProcessing(input_file, output_file_pre_processing);
+
+        ofstream opfs(output_file_pre_processing->name);
+        opfs << output_file_pre_processing->content;
+        opfs.close();
+
+        Assembler(output_file_pre_processing, output_file_assembler);
+
+        ofstream ofs(output_file_assembler->name);
+        ofs << output_file_assembler->content;
+        ofs.close();
+
+        delete input_file;
+        delete output_file_pre_processing;
+        delete output_file_assembler;
     }
-
-    PreProcessing(input_file, output_file_pre_processing);
-
-    ofstream opfs(output_file_pre_processing->name);
-    opfs << output_file_pre_processing->content;
-    opfs.close();
-
-    Assembler(output_file_pre_processing, output_file_assembler);
-
-    ofstream ofs(output_file_assembler->name);
-    ofs << output_file_assembler->content;
-    ofs.close();
-
-    delete input_file;
-    delete output_file_pre_processing;
-    delete output_file_assembler;
 }
