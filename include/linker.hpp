111c5dd905aaf263db14e1ff4be5ae657cae4b8c
André <andredornelas23@gmail.com>
2023-06-07 21:09:09 -0300

    cria header com assinaturas

---
+++ include/linker.hpp
@@ -0,0 +1,9 @@
+#ifndef LINKER_H
+#define LINKER_H
+
+#include "datatypes.hpp"
+
+void Link(std::vector<tokenMatrix> &input_matrixes, fileData *output_file);
+void SeparateMatrixes(std::vector<tokenMatrix> &input_matrixes, std::vector<objectData> &modules);
+
+#endif
