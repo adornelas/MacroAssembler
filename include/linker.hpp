f659dbbdf978ad342a18fc344008e649fd4a35d9
André <andredornelas23@gmail.com>
2023-06-08 12:17:56 -0300

    inclui include

--- include/linker.hpp
+++ include/linker.hpp
@@ -2,6 +2,7 @@
 #define LINKER_H
 
 #include "datatypes.hpp"
+#include "utils.hpp"
 
 void Link(std::vector<tokenMatrix> &input_matrixes, fileData *output_file);
 void SeparateMatrixes(std::vector<tokenMatrix> &input_matrixes, std::vector<objectData> &modules);
