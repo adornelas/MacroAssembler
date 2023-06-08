0ee1d528a127858c6dd34f12f2debdea4b084eb4
adornelas <andredornelas23@gmail.com>
2023-06-08 18:01:35 -0300

    troca && por ||

--- montador.cpp
+++ montador.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
     std::string name_pre_processed_file;
 
-    if (argc < 2 && argc > 5) {
+    if (argc < 2 || argc > 5) {
         cout << "Quantidade de argumentos invalida (" << argc << "),\ninsira até 4 arquivos no programa: nomedoprograma1.asm nomedoprograma2.asm nomedoprograma3.asm nomedoprograma4.asm" << endl;
         return 1;
     }
