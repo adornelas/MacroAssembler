530681bee5f9a31889970aa3ff30d6769c61fc84
André <andredornelas23@gmail.com>
2023-06-07 21:11:38 -0300

    cria esboço do processo de ligação

---
+++ src/linker.cpp
@@ -0,0 +1,17 @@
+#include "../include/linker.hpp"
+
+void Link(std::vector<tokenMatrix> &input_matrixes, fileData *output_file){
+
+    // Separar matrizes
+
+    if(input_matrixes.size() == 1){
+        // Apenas colocar no código de saída
+    }
+    else{
+        // Ligar
+    }
+}
+
+void SeparateMatrixes(std::vector<tokenMatrix> &input_matrixes, std::vector<objectData> &modules){
+
+}
