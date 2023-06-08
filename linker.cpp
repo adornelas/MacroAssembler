8562d5fc4df7cf43da7adb0afefcdb350f30539c
André <andredornelas23@gmail.com>
2023-06-07 21:02:08 -0300

    descomenta arquivo do ligador

--- linker.cpp
+++ linker.cpp
@@ -1,44 +1,43 @@
-// #include "include/datatypes.hpp"
-// #include "include/preprocessor.hpp"
-// #include "include/assembler.hpp"
+#include "include/datatypes.hpp"
+#include "include/preprocessor.hpp"
+#include "include/assembler.hpp"
 
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// int main(int argc, char** argv) {
-//     vector<tokenMatrix> input_matrix(argc - 1);
+int main(int argc, char** argv) {
+    vector<tokenMatrix> input_matrixes(argc - 1);
 
-//     if (argc < 2 && argc > 5) {
-//         cout << "Quantidade de argumentos invalida (" << argc << "),\ninsira até 4 arquivos no programa: nomedoprograma1.asm nomedoprograma2.asm nomedoprograma3.asm nomedoprograma4.asm" << endl;
-//         return 1;
-//     }
+    if (argc < 2 && argc > 5) {
+        cout << "Quantidade de argumentos invalida (" << argc << "),\ninsira até 4 arquivos no programa: nomedoprograma1 nomedoprograma2 nomedoprograma3 nomedoprograma4" << endl;
+        return 1;
+    }
 
-//     // TODO: aceitar arquivos de entrada sem extensão
-//     for (int i = 1; i < argc; i++) {
-//         fileData* input_file = new fileData{ .name = argv[i] };
-//         input_matrix.push_back({ .lines = 0 });
+    for (int i = 1; i < argc; i++) {
+        fileData* input_file = new fileData{ .name = argv[i] };
+        input_matrixes.push_back({ .lines = 0 });
 
-//         input_file->name.append(".obj");
-//         ifstream ifs(input_file->name);
-//         stringstream buffer;
-//         buffer << ifs.rdbuf();
-//         input_file->content = buffer.str();
-//         ConvertFileToMatrix(input_file, &input_matrix.at(i));
-//         delete input_file;
-//     }
+        input_file->name.append(".obj");
+        ifstream ifs(input_file->name);
+        stringstream buffer;
+        buffer << ifs.rdbuf();
+        input_file->content = buffer.str();
+        ConvertFileToMatrix(input_file, &input_matrixes.at(i));
+        delete input_file;
+    }
 
-//     fileData* output_file = new fileData{ .name = argv[1] };
-//     output_file->name.append(".exc");
+    fileData* output_file = new fileData{ .name = argv[1] };
+    output_file->name.append(".exc");
 
-//     // ligador
+    // ligador
 
-//     ofstream ofs(output_file->name);
-//     ofs << output_file->content;
-//     ofs.close();
+    ofstream ofs(output_file->name);
+    ofs << output_file->content;
+    ofs.close();
 
-//     delete output_file;
-// }
+    delete output_file;
+}
